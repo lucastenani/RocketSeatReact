141fe54c6dd7945543d1e3479bc70b2760c05e26
lucastenani <lucas.tenani3@gmail.com>
2022-11-09 11:58:14 -0300

    Styling CSS page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,10 +2,10 @@ import "./style.css";
 
 export function Home() {
   return (
-    <>
+    <div className="container">
       <h1>Attendance list</h1>
       <input type="text" placeholder="Enter a name..." />
       <button type="button">Add</button>
-    </>
+    </div>
   );
 }
