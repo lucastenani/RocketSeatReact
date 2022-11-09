d639741664afc1262883eda78fdfe292ddf57846
lucastenani <lucas.tenani3@gmail.com>
2022-11-08 22:18:12 -0300

    Add Fragment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <h1>Lista de Presença</h1>;
+  return (
+    <>
+      <h1>Lista de Presença</h1>
+      <input type="text" placeholder="Digite um nome..." />
+      <button type="button">Adicionar</button>
+    </>
+  );
 }
